522fa7812926983853d46b1764972bdc9336d76b
Nikos Fertakis <nikos@skroutz.gr>
2021-10-20 08:30:33 +0300

    Solve Day 14 part 2

--- lib/2020/day14.rb
+++ lib/2020/day14.rb
@@ -18,6 +18,15 @@ class Day14
     @memory.values.sum
   end
 
+  def part2
+    run_floating_program!
+    @memory.values.sum
+  end
+
+  def run_floating_program!
+    input.split("\n").each { |instruction| execute_floating!(instruction) }
+  end
+
   def run_program!
     input.split("\n").each { |instruction| execute!(instruction) }
   end
@@ -33,9 +42,37 @@ class Day14
     end
   end
 
+  def execute_floating!(instruction)
+    key, value = instruction.split(/\s*=\s*/, 2)
+    case key
+    when 'mask'
+      @mask = value
+    when /mem\[(\d+)\]/
+      addr = Regexp.last_match(1).to_i
+      masked_addr = memory_masked(addr, mask)
+      unfloat_address(masked_addr).each { |addr| @memory[addr] = value.to_i }
+    end
+  end
+
   # Applies the current mask to the value and then writes it to memory
   def set(address, value)
     memory[address] = masked(value, mask)
+  end
+
+  def memory_masked(address, mask)
+    mask_chars = mask.chars
+    nth_bit = 1 << mask.length # ensure padding
+    address_string = (address | nth_bit).to_s(2)[1..-1]
+    address_string.chars.each_with_index.reduce([]) do |coll, (c, i)|
+      coll << case mask_chars[i]
+              when 'X'
+                'X'
+              when '0'
+                c
+              when '1'
+                '1'
+              end
+    end.join
   end
 
   def masked(value, mask)
@@ -58,5 +95,30 @@ class Day14
     end
 
     value & zeros
+  end
+
+  def unfloat_address(address)
+    base = address.tr('X', '0').to_i(2)
+    floating = floating_bits(address)
+    perms = 2**floating.length
+
+    # Trick so we always have 0 padding!
+    nth_bit = 1 << floating.length
+    (0...perms).map do |perm|
+      bitmap = (perm | nth_bit).to_s(2)[1..-1]
+      num = base
+      bitmap.chars.each_with_index do |c, i|
+        next if c == '0'
+
+        num += 2**floating[i]
+      end
+      num
+    end
+  end
+
+  def floating_bits(address)
+    address.chars.reverse.each_with_index.reduce([]) do |coll, (bit, idx)|
+      bit == 'X' ? coll.append(idx) : coll
+    end
   end
 end
--- spec/2020/day14_spec.rb
+++ spec/2020/day14_spec.rb
@@ -4,7 +4,7 @@ require 'rspec'
 require '2020/day14'
 
 describe 'Day 14' do
-  describe '#part!' do
+  describe '#part1!' do
     let(:input) do
       <<~TXT
         mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
@@ -18,6 +18,53 @@ describe 'Day 14' do
 
     it 'executes the program and counts values in memory' do
       expect(solver.part1).to eq(165)
+    end
+  end
+
+  describe '#part2!' do
+    let(:input) do
+      <<~TXT
+        mask = 000000000000000000000000000000X1001X
+        mem[42] = 100
+        mask = 00000000000000000000000000000000X0XX
+        mem[26] = 1
+      TXT
+    end
+
+    let(:solver) { Day14.new(input) }
+
+    it 'executes the program and counts values in memory' do
+      expect(solver.part2).to eq(208)
+    end
+  end
+
+
+  describe '#run_floating_program!' do
+    let(:input) do
+      <<~TXT
+        mask = 000000000000000000000000000000X1001X
+        mem[42] = 100
+        mask = 00000000000000000000000000000000X0XX
+        mem[26] = 1
+      TXT
+    end
+
+    let(:solver) { Day14.new(input) }
+
+    it 'runs the whole input program' do
+      solver.run_floating_program!
+      expect(solver.mask).to eq('00000000000000000000000000000000X0XX')
+      expect(solver.memory[58]).to eq(100)
+      expect(solver.memory[59]).to eq(100)
+
+      expect(solver.memory[16]).to eq(1)
+      expect(solver.memory[17]).to eq(1)
+      expect(solver.memory[18]).to eq(1)
+      expect(solver.memory[19]).to eq(1)
+      expect(solver.memory[24]).to eq(1)
+      expect(solver.memory[25]).to eq(1)
+      expect(solver.memory[26]).to eq(1)
+      expect(solver.memory[27]).to eq(1)
     end
   end
 
@@ -38,6 +85,30 @@ describe 'Day 14' do
       expect(solver.mask).to eq('XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X')
       expect(solver.memory[7]).to eq(101)
       expect(solver.memory[8]).to eq(64)
+    end
+  end
+
+  describe '#execute_floating!' do
+    let(:solver) { Day14.new }
+
+    context 'when mask' do
+      it 'sets mask accordingly' do
+        instruction = 'mask = 000000000000000000000000000000X1001X'
+        expect { solver.execute_floating!(instruction) }.to change { solver.mask }.from('X' * 36).to('000000000000000000000000000000X1001X')
+      end
+    end
+
+    context 'when mem' do
+      before { solver.mask = '000000000000000000000000000000X1001X' }
+
+      it 'saves the appropriate value to memory' do
+        instruction = 'mem[42] = 100'
+        solver.execute_floating!(instruction)
+        expect(solver.memory[26]).to eq(100)
+        expect(solver.memory[27]).to eq(100)
+        expect(solver.memory[58]).to eq(100)
+        expect(solver.memory[59]).to eq(100)
+      end
     end
   end
 
@@ -87,6 +158,34 @@ describe 'Day 14' do
     it 'sets a value after masking it' do
       solver.set(8, 0)
       expect(solver.memory[8]).to eq(64)
+    end
+  end
+
+  describe '#unfloat_address' do
+    let(:solver) { Day14.new }
+
+    it 'returns the array of possible addresses' do
+      expect(solver.unfloat_address('000000000000000000000000000000X1101X')).to match_array([26, 27, 58, 59])
+    end
+
+    it 'returns the array of possible addresses' do
+      expect(solver.unfloat_address('00000000000000000000000000000001X0XX')).to match_array([16, 17, 18, 19, 24, 25, 26, 27])
+    end
+  end
+
+  describe '#floating_bits' do
+    let(:solver) { Day14.new }
+
+    it 'returns the significant indexes of the floating bits' do
+      expect(solver.floating_bits('000000000000000000000000000000X1101X')).to match_array([0, 5])
+    end
+  end
+
+  describe 'memory_masked' do
+    let(:solver) { Day14.new }
+
+    it 'applies the mask to the memory address' do
+      expect(solver.memory_masked(42, '000000000000000000000000000000X1001X')).to eq('000000000000000000000000000000X1101X')
     end
   end
 end
